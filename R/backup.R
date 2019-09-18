373ab6eac8791ceb5823585216d521e7b57ccab3
Michael Pinus <pinusm@post.bgu.ac.il>
2019-09-18 17:57:09 +0300

    finish documentation

--- R/backup.R
+++ R/backup.R
@@ -1,15 +1,36 @@
 
+#' List all installed packages
+#'
+#' @return A data frame with installed packages names and version numbers
+#'
+
 list_packages <- function(){
   ip <- as.data.frame(utils::installed.packages()[,c(1,3:4)])
   rownames(ip) <- NULL
   ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
 }
+
+#' Filter CRAN packages
+#'
+#' Takes a data frame of package names and versions (i.e. The output of \code{list_packages()}), and return only the names of packages that are available on CRAN.
+#'
+#' @param packages a data frame of package names and versions (i.e. The output of \code{list_packages()})
+#'
+#' @return a vector of package names
+#'
+
 list_cran_packages <- function(packages) {
     pkgs <- packages %>% dplyr::pull(1) %>% as.character()
     cran_available_packages <- rownames(utils::available.packages())
     pkgs[pkgs %in% cran_available_packages]
 }
 
+#' Filter GitHub packages
+#'
+#' Returns a vector with the name and repo composites (e.g. "pinusm/commuteR") for packages that were installed from GitHub.
+#'
+#' @return A vector with the name and repo composites, for packages that were installed from GitHub.
+#'
 list_github_packages <- function(){
   pkgs <- utils::installed.packages(fields = "RemoteType")
   github_pkgs <- pkgs[pkgs[, "RemoteType"] %in% "github", "Package"]
@@ -24,6 +45,13 @@ list_github_packages <- function(){
   github_pkgs_repos
 }
 
+#' Gathers the list of objects to backup and upload to the cloud
+#'
+#' Currently, a list of all installed packages, a vector of all CRAN packages, and a vector of all GitHub
+#'     packages, in a repo/packagename composite.
+#' @return A list of objects to backup and upload to the cloud
+#'
+
 populate_backup <- function(){
   packages <- list_packages()
   gh_packages <- list_github_packages()
@@ -36,6 +64,11 @@ populate_backup <- function(){
   to_backup
 }
 
+#' Backup to cloud
+#'
+#' @param path a folder in Dropbox where the backup files are to be saved (typically, the same \code{path} given to \code{restore_from_cloud()}
+#' @export
+#'
 backup_to_cloud <- function(path = "commuteR"){
   packages <- list_packages()
   gh_packages <- list_github_packages()

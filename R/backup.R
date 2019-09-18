7655e8ea656798015af54654d1ba1f2678e05477
Michael Pinus <pinusm@post.bgu.ac.il>
2019-09-18 18:04:04 +0300

    adhere to tidyverse styleguides with  stylr::style_pkg()

--- R/backup.R
+++ R/backup.R
@@ -4,10 +4,10 @@
 #' @return A data frame with installed packages names and version numbers
 #'
 
-list_packages <- function(){
-  ip <- as.data.frame(utils::installed.packages()[,c(1,3:4)])
+list_packages <- function() {
+  ip <- as.data.frame(utils::installed.packages()[, c(1, 3:4)])
   rownames(ip) <- NULL
-  ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
+  ip <- ip[is.na(ip$Priority), 1:2, drop = FALSE]
 }
 
 #' Filter CRAN packages
@@ -20,9 +20,11 @@ list_packages <- function(){
 #'
 
 list_cran_packages <- function(packages) {
-    pkgs <- packages %>% dplyr::pull(1) %>% as.character()
-    cran_available_packages <- rownames(utils::available.packages())
-    pkgs[pkgs %in% cran_available_packages]
+  pkgs <- packages %>%
+    dplyr::pull(1) %>%
+    as.character()
+  cran_available_packages <- rownames(utils::available.packages())
+  pkgs[pkgs %in% cran_available_packages]
 }
 
 #' Filter GitHub packages
@@ -31,14 +33,14 @@ list_cran_packages <- function(packages) {
 #'
 #' @return A vector with the name and repo composites, for packages that were installed from GitHub.
 #'
-list_github_packages <- function(){
+list_github_packages <- function() {
   pkgs <- utils::installed.packages(fields = "RemoteType")
   github_pkgs <- pkgs[pkgs[, "RemoteType"] %in% "github", "Package"]
   github_pkgs_repos <- c("")
 
-  for (i in seq_along(github_pkgs)){
-    repo = utils::packageDescription(github_pkgs[i], fields = "GithubRepo")
-    username = utils::packageDescription(github_pkgs[i], fields = "GithubUsername")
+  for (i in seq_along(github_pkgs)) {
+    repo <- utils::packageDescription(github_pkgs[i], fields = "GithubRepo")
+    username <- utils::packageDescription(github_pkgs[i], fields = "GithubUsername")
 
     github_pkgs_repos[i] <- paste0(username, "/", repo)
   }
@@ -52,14 +54,15 @@ list_github_packages <- function(){
 #' @return A list of objects to backup and upload to the cloud
 #'
 
-populate_backup <- function(){
+populate_backup <- function() {
   packages <- list_packages()
   gh_packages <- list_github_packages()
   cran_packages <- list_cran_packages(packages)
 
-  to_backup <- list(  packages = packages
-                      , gh_packages = gh_packages
-                      , cran_packages = cran_packages
+  to_backup <- list(
+    packages = packages,
+    gh_packages = gh_packages,
+    cran_packages = cran_packages
   )
   to_backup
 }
@@ -69,29 +72,28 @@ populate_backup <- function(){
 #' @param path a folder in Dropbox where the backup files are to be saved (typically, the same \code{path} given to \code{restore_from_cloud()}
 #' @export
 #'
-backup_to_cloud <- function(path = "commuteR"){
-
+backup_to_cloud <- function(path = "commuteR") {
   to_backup <- populate_backup()
 
   temp_path <- tempdir()
 
-  paths_of_backups <- lapply(seq_along(to_backup), function(i){
-    paste0(temp_path,"\\",names(to_backup)[[i]],".Rdata")
-    }) %>% unlist
+  paths_of_backups <- lapply(seq_along(to_backup), function(i) {
+    paste0(temp_path, "\\", names(to_backup)[[i]], ".Rdata")
+  }) %>% unlist()
 
-  #authenticate
+  # authenticate
   rdrop2::drop_auth()
 
-  #create actual files
+  # create actual files
   for (i in seq_along(to_backup)) {
-    dput(to_backup[[i]],paths_of_backups[i])
+    dput(to_backup[[i]], paths_of_backups[i])
   }
 
-  #upload to cloud service of choice
+  # upload to cloud service of choice
   if (!rdrop2::drop_exists(path = path)) {
     rdrop2::drop_create(path = path)
   }
   for (i in seq_along(to_backup)) {
-    rdrop2::drop_upload(file = paths_of_backups[i] , path=path)
+    rdrop2::drop_upload(file = paths_of_backups[i], path = path)
   }
 }
--- R/restore.R
+++ R/restore.R
@@ -3,10 +3,10 @@
 #' @return A data frame with the names and versions of all the packages installed
 #' @import utils
 
-list_packages <- function(){
-  ip <- as.data.frame(utils::installed.packages()[,c(1,3:4)])
+list_packages <- function() {
+  ip <- as.data.frame(utils::installed.packages()[, c(1, 3:4)])
   rownames(ip) <- NULL
-  ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
+  ip <- ip[is.na(ip$Priority), 1:2, drop = FALSE]
 }
 
 #' Filter package names of packages that already installed
@@ -34,12 +34,12 @@ filter_installed_packages <- function(packages) {
 #' @import pacman
 #'
 
-install_cran_packages <- function(packages, dependencies = NA, quiet = T, force = F, lib = .libPaths()[1]){
+install_cran_packages <- function(packages, dependencies = NA, quiet = T, force = F, lib = .libPaths()[1]) {
   # old_repos <- getOption("repos")
   # options(repos = getOption("repos")["CRAN"])
   for (i in seq_along(packages)) {
     pkg <- packages[i] %>% as.character()
-    pacman::p_install(pkg, force = force , dependencies = dependencies , quiet = quiet,character.only = T, lib = lib, repos = getOption("repos")["CRAN"])
+    pacman::p_install(pkg, force = force, dependencies = dependencies, quiet = quiet, character.only = T, lib = lib, repos = getOption("repos")["CRAN"])
   }
 }
 
@@ -56,10 +56,10 @@ install_cran_packages <- function(packages, dependencies = NA, quiet = T, force 
 #' @param lib character vector giving the library directories where to install the packages. Recycled as needed. If missing, defaults to the first element of .libPaths().
 #' @import pacman
 
-install_github_packages <- function(packages, dependencies = NA, quiet = T, force = F, lib = .libPaths()[1]){
+install_github_packages <- function(packages, dependencies = NA, quiet = T, force = F, lib = .libPaths()[1]) {
   for (i in seq_along(packages)) {
     pkg <- packages[i] %>% as.character()
-    remotes::install_github(pkg, force = force , dependencies = dependencies , quiet = quiet , lib = lib)
+    remotes::install_github(pkg, force = force, dependencies = dependencies, quiet = quiet, lib = lib)
   }
 }
 
@@ -69,7 +69,7 @@ install_github_packages <- function(packages, dependencies = NA, quiet = T, forc
 #' @param cran_packages A string vector with names of packages to be installed from CRAN
 #' @param gh_packages A string vector with names of repo\\packages to be installed from GitHub
 #'
-install_packages_from_backup <- function(cran_packages , gh_packages){
+install_packages_from_backup <- function(cran_packages, gh_packages) {
   filtered_cran_package <- filter_installed_packages(cran_packages)
   filtered_gh_package <- filter_installed_packages(gh_packages)
 
@@ -85,8 +85,11 @@ install_packages_from_backup <- function(cran_packages , gh_packages){
 #' @import rdrop2
 #'
 
-list_backup <- function(path = path){
-  rdrop2::drop_dir(path) %>% dplyr::pull(name) %>% base::basename() %>% tools::file_path_sans_ext()
+list_backup <- function(path = path) {
+  rdrop2::drop_dir(path) %>%
+    dplyr::pull(name) %>%
+    base::basename() %>%
+    tools::file_path_sans_ext()
 }
 
 #' Restore from cloud (Install packages)
@@ -94,30 +97,31 @@ list_backup <- function(path = path){
 #' @param path a folder in Dropbox where the backup files where saved (typically, the same \code{path} given to \code{backup_to_cloud()}
 #' @export
 
-restore_from_cloud <- function(path = "commuteR"){
-  #authenticate
+restore_from_cloud <- function(path = "commuteR") {
+  # authenticate
   rdrop2::drop_auth()
 
   temp_path <- tempdir()
 
   list_of_backs <- list_backup(path = path)
 
-  paths_of_restores <- lapply(seq_along(list_of_backs), function(i){
-    paste0(temp_path,"\\",list_of_backs[[i]], ".Rdata")
-  }) %>% unlist
+  paths_of_restores <- lapply(seq_along(list_of_backs), function(i) {
+    paste0(temp_path, "\\", list_of_backs[[i]], ".Rdata")
+  }) %>% unlist()
 
-  #get actual files
+  # get actual files
   for (i in seq_along(paths_of_restores)) {
-    rdrop2::drop_download(path = paste0(path,"/",list_of_backs[[i]],".Rdata")
-                          , local_path = paths_of_restores[i]
-                          , overwrite = T)
+    rdrop2::drop_download(
+      path = paste0(path, "/", list_of_backs[[i]], ".Rdata"),
+      local_path = paths_of_restores[i],
+      overwrite = T
+    )
   }
 
-  #load actual files
+  # load actual files
   for (i in seq_along(paths_of_restores)) {
     assign(list_of_backs[[i]], dget(paths_of_restores[i]))
   }
 
-  install_packages_from_backup(cran_packages = cran_packages , gh_packages = gh_packages)
+  install_packages_from_backup(cran_packages = cran_packages, gh_packages = gh_packages)
 }
-
